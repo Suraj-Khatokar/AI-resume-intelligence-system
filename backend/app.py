348e546841d99c7b159d989fc960d9f7aada1c6a
Suraj K S <suraj200527@gmail.com>
2026-02-19 20:00:56 +0530

    connected to flask backend to pastgreSQL database

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,28 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)
 
+# Database connection function
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="ai_resume_dg",
+        user="postgres",
+        password="user"
+    )
+    return conn
+
 @app.route("/")
 def home():
-    return jsonify({"message": "AI Resume Intelligence Backend Running"})
+    try:
+        conn = get_db_connection()
+        conn.close()
+        return jsonify({"message": "Backend connected to PostgreSQL successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(debug=True)
